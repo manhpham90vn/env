f565e52fadeed00ca5a239d0ba6185ad3bf7fa75
Manh Pham <manhpham90vn@icloud.com>
2022-02-20 10:59:51 +0700

    exit

--- setup.sh
+++ setup.sh
@@ -25,11 +25,13 @@ echo "Starting setup"
 if ! [ -d ~/.oh-my-zsh ]; then
 	echo "Installing oh-my-zsh"
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    exit
 fi
 
 if test ! $(which brew); then
     echo "Installing homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    exit
 fi
 
 echo "Config XCode..."
